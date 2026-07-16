eb519288d75b2262fd2bd05683e62b3fe5f66dde
gaoyantao <gaoyantao@pwmat.com>
2026-07-16 18:13:21 +0800

    debug: show Sep 30 equity curve data via alert()

--- ui/src/pages/BacktestPage.tsx
+++ ui/src/pages/BacktestPage.tsx
@@ -638,14 +638,13 @@ function EquityCurveChart({ result, initialCapital, quotes }: { result: Backtest
    const start = Math.max(0, sep30 - 3);
    const end = Math.min(eqNum.length - 1, sep30 + 3);
    const b0 = eqNum[0]?.value || 100000;
-   let logLines = "[EQ DEBUG] equity data around 2024-09-30:\\n";
+   let logLines = "[EQ DEBUG] 2024-09-30 前后权益曲线数据:\\n";
    for (let d = start; d <= end; d++) {
      const pct = ((eqNum[d].value - b0) / b0 * 100).toFixed(4);
      logLines += "  [" + d + "] " + eqNum[d].time + " val=" + eqNum[d].value + " pct=" + pct + "%\\n";
    }
-   logLines += "[EQ DEBUG] total_return=" + result.total_return + "\\n";
-   logLines += "[EQ DEBUG] initialCapital=" + initialCapital + "\\n";
-   localStorage.setItem("stockmate_eq_debug", logLines);
+   logLines += "total_return=" + result.total_return + "% initialCapital=" + initialCapital;
+   alert(logLines);
  }
  const firstVal = eqNum.length > 0 ? eqNum[0].value : initialCapital;
  const base = firstVal > 0 ? firstVal : 1;
